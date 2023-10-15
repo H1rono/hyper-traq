4278818787e1343d08fa3cbabeafceec25f09089
H1rono <hronok66@gmail.com>
2023-10-15 20:04:48 +0900

    Write example

---
+++ examples/get-users.rs
@@ -0,0 +1,11 @@
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let access_token = std::env::var("ACCESS_TOKEN")?;
+    let client = hyper_traq::Client::builder()
+        .authorization_bearer(&access_token)
+        .build();
+    let req = hyper_traq::apis::users::GetUsers;
+    let res = client.request(req).await?;
+    println!("{}", res);
+    Ok(())
+}
