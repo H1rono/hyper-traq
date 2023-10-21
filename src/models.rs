fa6d6fd89b7aca901475ac8924c21a78c3570b79
H1rono <hronok66@gmail.com>
2023-10-21 12:01:57 +0900

    pub model fields

--- src/models.rs
+++ src/models.rs
@@ -6,13 +6,13 @@ use uuid::Uuid;
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
-    id: Uuid,
-    name: String,
-    display_name: String,
-    icon_file_id: Uuid,
-    bot: bool,
-    state: i32,
-    updated_at: String,
+    pub id: Uuid,
+    pub name: String,
+    pub display_name: String,
+    pub icon_file_id: Uuid,
+    pub bot: bool,
+    pub state: i32,
+    pub updated_at: String,
 }
 
 pub type Users = Vec<User>;
@@ -29,28 +29,28 @@ pub enum UserAccountState {
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct UserTag {
-    tag_id: Uuid,
-    tag: String,
-    is_locked: bool,
-    created_at: String,
-    updated_at: String,
+    pub tag_id: Uuid,
+    pub tag: String,
+    pub is_locked: bool,
+    pub created_at: String,
+    pub updated_at: String,
 }
 
 /// https://github.com/traPtitech/traQ/blob/bf768fc1d4ce1d5eb1575dd64f008f70f97087dd/router/v3/responses.go#L110-L124
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct UserDetail {
-    id: Uuid,
-    state: UserAccountState,
-    bot: bool,
-    icon_file_id: Uuid,
-    display_name: String,
-    name: String,
-    twitter_id: String,
-    last_online: Option<String>,
-    updated_at: String,
-    tags: Vec<UserTag>,
-    groups: Vec<Uuid>,
-    bio: String,
-    home_channel: Option<Uuid>,
+    pub id: Uuid,
+    pub state: UserAccountState,
+    pub bot: bool,
+    pub icon_file_id: Uuid,
+    pub display_name: String,
+    pub name: String,
+    pub twitter_id: String,
+    pub last_online: Option<String>,
+    pub updated_at: String,
+    pub tags: Vec<UserTag>,
+    pub groups: Vec<Uuid>,
+    pub bio: String,
+    pub home_channel: Option<Uuid>,
 }
