88beeb658b0f15f30a4849717e29f45d129efce7
H1rono <hronok66@gmail.com>
2023-10-21 11:27:52 +0900

    Use uuid

--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,13 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
-    id: String,
+    id: Uuid,
     name: String,
     display_name: String,
-    icon_file_id: String,
+    icon_file_id: Uuid,
     bot: bool,
     state: i32,
     updated_at: String,
